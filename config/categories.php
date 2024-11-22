56dbac09670fe7a4866f08a8154c778bffc63895
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-11-22 16:52:02 +0100

    creati config restaurant, dishes, categories

---
+++ config/categories.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+   [
+    "name" => "Ristorante Cinese",
+   ],
+   [
+    "name" => "Ristorante Giapponese",
+   ],
+   [
+    "name" => "Ristorante Coreano",
+   ],
+   [
+    "name" => "Ristorante Messicano",
+   ],
+   [
+    "name" => "Pizzeria",
+   ],
+   [
+    "name" => "Ristorante Fusion",
+   ],
+   [
+    "name" => "Ristorante Italiano",
+   ],
+   [
+    "name" => "Turkish Kebap",
+   ],
+   [
+    "name" => "Gelateria",
+   ],
+   [
+    "name" => "Pasticceria",
+   ],
+
+]
+
+
+
+
+?>
