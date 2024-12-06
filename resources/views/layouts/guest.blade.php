0a2adbbcddbaa0fd153b39885fa4a631c2b5d1cf
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-12-06 15:10:50 +0100

    sistemato file guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,25 +9,33 @@
 
         <!-- Scripts -->
         @vite('resources/js/app.js')
-        
-        <!--Adobe Font-->
-        <link rel="stylesheet" href="https://use.typekit.net/lyi7tbf.css">
-
-
     </head>
     <body>
         <header>
-            <nav class="navbar shadow navbar-expand-lg d-none">
-                <div class="container ">
-                    
+            <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+                <div class="container">
+                    <a class="navbar-brand" href="/">Template</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarText">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             @auth
                                 <li class="nav-item">
-                                    <img class="logo-nav" src="{{ asset('../storage/images/Logo-deliveBoo.svg') }}" alt="">
+                                    <a class="nav-link" href="{{ route('admin.dashboard') }}">Dashboard</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link 2</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link 3</a>
+                                </li>
+                            @else
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">Register</a>
                                 </li>
                             @endauth
                         </ul>
@@ -36,7 +44,7 @@
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
-                                <button type="submit" class="logout button-menu">
+                                <button type="submit" class="btn btn-outline-danger">
                                     Log Out
                                 </button>
                             </form>
@@ -46,20 +54,10 @@
             </nav>
         </header>
 
-        <main class="">
+        <main class="py-4">
             <div class="container">
                 @yield('main-content')
             </div>
         </main>
     </body>
 </html>
-
-<style>
-    .logout{
-        font-size: 15px;
-    }
-    .navbar{
-  background-color: white;
-  box-shadow: 
- }
-</style>
